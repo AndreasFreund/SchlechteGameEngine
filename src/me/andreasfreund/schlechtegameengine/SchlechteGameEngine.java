9373713408e19758d871d4514072f040412743a2
Andreas Freund <freund.andreas@web.de>
2015-02-24 16:44:22 +0100

    ABC

--- src/me/andreasfreund/schlechtegameengine/SchlechteGameEngine.java
+++ src/me/andreasfreund/schlechtegameengine/SchlechteGameEngine.java
@@ -1,11 +1,10 @@
 package me.andreasfreund.schlechtegameengine;
 
 public class SchlechteGameEngine {
-    private int test, test2, test3;
-    private boolean test5;
-    
-    public SchlechteGameEngine()
-    {
-    	this.test = 3;
-    }
+	private int test, test2, test3;
+	private boolean test5;
+
+	public SchlechteGameEngine() {
+		this.test = 3;
+	}
 }
