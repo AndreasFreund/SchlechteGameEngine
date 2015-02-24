185eb4a2e790bebd6b01b3b6aca82a6be1e25b05
Florian <Florian@192.168.2.104>
2015-02-24 16:31:49 +0100

    Test

--- src/me/andreasfreund/schlechtegameengine/SchlechteGameEngine.java
+++ src/me/andreasfreund/schlechtegameengine/SchlechteGameEngine.java
@@ -3,4 +3,9 @@ package me.andreasfreund.schlechtegameengine;
 public class SchlechteGameEngine {
     private int test, test2, test3;
     private boolean test5;
+    
+    public SchlechteGameEngine()
+    {
+    	this.test = 3;
+    }
 }
