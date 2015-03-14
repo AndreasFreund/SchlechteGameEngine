e401122c2b776d26161ca55e0376826a94bd4e80
Florian <Florian@192.168.2.102>
2015-03-14 16:59:22 +0100

    Added Transparency

--- src/me/andreasfreund/schlechtegameengine/SchlechteGameEngine.java
+++ src/me/andreasfreund/schlechtegameengine/SchlechteGameEngine.java
@@ -61,6 +61,8 @@ public class SchlechteGameEngine {
 	private void mainloop() {
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		while (!this.window.isCloseRequested()) {
 			GL11.glClearColor(0, 0, 0, 0);
 			GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT);
