67108560dd627f7a661e71459445d3274598dc92
Andreas Freund <freund.andreas@web.de>
2015-03-14 15:55:55 +0100

    Fixed Keys not being recognized if pressed for a long time

--- src/me/andreasfreund/schlechtegameengine/Keyboard.java
+++ src/me/andreasfreund/schlechtegameengine/Keyboard.java
@@ -4,16 +4,15 @@ import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWKeyCallback;
 
 public class Keyboard extends GLFWKeyCallback {
-	
+
 	private boolean[] keys = new boolean[400];
-	
+
 	@Override
 	public void invoke(long arg0, int arg1, int arg2, int arg3, int arg4) {
-		this.keys[arg1] = arg3 == GLFW.GLFW_PRESS;
+		this.keys[arg1] = arg3 == GLFW.GLFW_PRESS || arg3 == GLFW.GLFW_REPEAT;
 	}
 
-	public boolean isKeyDown(int key)
-	{
+	public boolean isKeyDown(int key) {
 		return this.keys[key];
 	}
 }
